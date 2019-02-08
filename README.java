91d723f36a76771973f1b326a9b4f0f6f67f93ff
saravana <saravanasaru14313@gmail.com>
2019-02-08 14:30:15 +0530

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+class Largestsmallest
+{
+ public static void main(String args[])
+ {
+	Scanner input=new Scanner(System.in);
+	int a=input.nextInt();
+	int arr[]=new int[a];
+	for(int i=0;i<a;i++)
+	{
+		arr[i]=input.nextInt();
+		System.out.println("");
+	}
+        int min=arr[0];
+	for(int j=1;j<a;j++)
+	{
+		if(min>arr[j])
+		{
+			min=arr[j];
+		}
+	}
+	System.out.println(min);
+	int max=arr[0];
+	for(int k=1;k<a;k++)
+	{
+		if(max<arr[k])
+		{
+			max=arr[k];
+		}
+	}
+	System.out.println(max);
+	
+	
+ }
+}
